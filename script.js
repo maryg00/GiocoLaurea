2994e1deb8a1bf163037db242ce3016969ed4f3c
maryg00 <marygiac3@gmail.com>
2025-03-28 13:18:29 +0100

    script.js

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-/*const animals = ["Leone", "Tigre", "Giraffa", "Elefante", "Lupo", "Panda", "Volpe", "Aquila", "Orso", "Delfino"];
-let assignedAnimals = {};
-
-function assignAnimal() {
-    let availableAnimals = animals.filter(animal => (assignedAnimals[animal] || 0) < 2);
-    
-    if (availableAnimals.length === 0) {
-        document.getElementById("result").innerText = "Tutti gli animali sono stati assegnati!";
-        return;
-    }
-
-    let randomAnimal = availableAnimals[Math.floor(Math.random() * availableAnimals.length)];
-    assignedAnimals[randomAnimal] = (assignedAnimals[randomAnimal] || 0) + 1;
-
-    document.getElementById("result").innerText = "Il tuo animale è: " + randomAnimal;
-}*/
-
-
 const animals = ["Cane", "Gatto", "Delfino", "Balena", "Foca", "Pesce", "Coccodrillo", "Mucca", "Pecora", "Colomba", "Civetta"];
 let assignedAnimals = {};
 let assignedOnce = [];
